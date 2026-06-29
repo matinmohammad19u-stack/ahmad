e912b637afd49393f2fac351d5f976c59584b35f
matinmohammad19u-stack <matinmohammad19u@gmail.com>
2026-06-29 13:22:14 +0330

    Update ship_system.py

--- ship_system.py
+++ ship_system.py
@@ -1,33 +1,162 @@
-# ship_system.py
-
 ships = {
     "Small Boat": {
         "price": 0,
         "speed": 1,
-        "required_level": 1
+        "durability": 1,
+        "cargo": 5,
+        "required_level": 1,
+        "description": "یه قایق ساده برای شروع"
     },
-
     "Fishing Boat": {
         "price": 500,
         "speed": 2,
-        "required_level": 20
+        "durability": 2,
+        "cargo": 10,
+        "required_level": 20,
+        "description": "قایق ماهیگیری با ظرفیت بیشتر"
     },
-
     "Pirate Ship": {
         "price": 2000,
         "speed": 3,
-        "required_level": 50
+        "durability": 3,
+        "cargo": 20,
+        "required_level": 50,
+        "description": "کشتی دزدان دریایی"
     },
-
     "Grand Ship": {
         "price": 8000,
         "speed": 4,
-        "required_level": 120
+        "durability": 4,
+        "cargo": 35,
+        "required_level": 120,
+        "description": "کشتی بزرگ گرند لاین"
     },
-
     "Warship": {
         "price": 20000,
         "speed": 5,
-        "required_level": 250
-    }
+        "durability": 5,
+        "cargo": 50,
+        "required_level": 250,
+        "description": "کشتی جنگی نیروی دریایی"
+    },
+    "Caravel": {
+        "price": 12000,
+        "speed": 4,
+        "durability": 3,
+        "cargo": 40,
+        "required_level": 150,
+        "description": "کشتی تجاری سریع"
+    },
+    "Galleon": {
+        "price": 18000,
+        "speed": 4,
+        "durability": 5,
+        "cargo": 60,
+        "required_level": 200,
+        "description": "کشتی سنگین با ظرفیت بالا"
+    },
+    "Sunny Go": {
+        "price": 35000,
+        "speed": 6,
+        "durability": 6,
+        "cargo": 70,
+        "required_level": 350,
+        "description": "کشتی افسانه‌ای استراهت‌ها"
+    },
+    "Thousand Sunny Upgrade": {
+        "price": 50000,
+        "speed": 7,
+        "durability": 7,
+        "cargo": 85,
+        "required_level": 420,
+        "description": "Sunny Go با Coup de Burst آپگرید شده"
+    },
+    "Going Merry": {
+        "price": 25000,
+        "speed": 5,
+        "durability": 4,
+        "cargo": 55,
+        "required_level": 300,
+        "description": "کشتی اول استراهت‌ها، سریع و چابک"
+    },
+    "Thriller Bark Ship": {
+        "price": 55000,
+        "speed": 6,
+        "durability": 8,
+        "cargo": 90,
+        "required_level": 450,
+        "description": "کشتی عظیم موریا"
+    },
+    "Big Mom Ship": {
+        "price": 65000,
+        "speed": 7,
+        "durability": 8,
+        "cargo": 100,
+        "required_level": 500,
+        "description": "کشتی بیگ مام با پوشش هوموز"
+    },
+    "Moby Dick": {
+        "price": 80000,
+        "speed": 7,
+        "durability": 9,
+        "cargo": 110,
+        "required_level": 550,
+        "description": "کشتی وایتبیرد"
+    },
+    "Queen Mama Chanter": {
+        "price": 70000,
+        "speed": 7,
+        "durability": 8,
+        "cargo": 105,
+        "required_level": 520,
+        "description": "کشتی اصلی ناوگان بیگ مام"
+    },
+    "Beasts Pirates Ship": {
+        "price": 75000,
+        "speed": 6,
+        "durability": 10,
+        "cargo": 120,
+        "required_level": 560,
+        "description": "کشتی زرهی کایدو"
+    },
+    "Navy Battleship": {
+        "price": 90000,
+        "speed": 8,
+        "durability": 9,
+        "cargo": 115,
+        "required_level": 580,
+        "description": "کشتی جنگی پیشرفته نیروی دریایی"
+    },
+    "Polar Tang": {
+        "price": 85000,
+        "speed": 8,
+        "durability": 8,
+        "cargo": 100,
+        "required_level": 570,
+        "description": "زیردریایی لاو"
+    },
+    "Oro Jackson": {
+        "price": 150000,
+        "speed": 9,
+        "durability": 10,
+        "cargo": 150,
+        "required_level": 700,
+        "description": "کشتی افسانه‌ای راجر، بهترین کشتی دنیا"
+    },
+    "Red Force": {
+        "price": 130000,
+        "speed": 9,
+        "durability": 9,
+        "cargo": 130,
+        "required_level": 650,
+        "description": "کشتی شانکس، یکی از قدرتمندترین کشتی‌های دنیا"
+    },
+    "Dark King Ship": {
+        "price": 110000,
+        "speed": 8,
+        "durability": 9,
+        "cargo": 120,
+        "required_level": 620,
+        "description": "کشتی ریلی، نایب پادشاه دزدان"
+    },
 }
